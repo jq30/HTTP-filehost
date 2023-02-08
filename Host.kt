83ab9b3e3d2dbb7c4a5e8fdc90558fd37fe99c0b
Jing Qian <jqian30@stuy.edu>
2023-02-07 19:15:20 -0500

    aaaaaaa

---
+++ Host.kt
@@ -0,0 +1,37 @@
+import java.net.*;
+import java.io.*;
+
+val PORT = 80;
+
+fun main(args: Array<String>) {
+  if (args.size < 1) {
+    println("please specify file.");
+    System.exit(1);
+  }
+
+  val file = File(args[0]);
+  val fileName = file.getName();
+
+  val server = ServerSocket(PORT);
+  println("server listening on port $PORT...");
+  val clientSocket = server.accept(); //blocks
+
+  val responseHeader = "HTTP/1.1 200 OK\r\nContent-Disposition: attachment; filename=\"$fileName\"\r\n\r\n";
+
+  val read = FileInputStream(file);
+  val write = clientSocket.getOutputStream();
+
+  write.write(responseHeader.toByteArray());
+
+  val buffer = ByteArray(1024) {0};
+  var bytesread = read.read(buffer);
+  while (bytesread != -1) {
+    write.write(buffer, 0, bytesread);
+    bytesread = read.read(buffer);
+  }
+
+  write.write("\r\n\r\n".toByteArray());
+  clientSocket.close();
+
+  println("done, exiting...");
+}
